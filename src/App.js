bc2c653d90625301ed9a08a5de2c6b4707cebfe7
mariansilva <marian1996silva@gmail.com>
2026-02-17 13:07:26 +0530

    Food Delivery App with Navigation Bar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './components/navBar/NavBar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <NavBar/>
+      <title>Food Del</title>
+    </div>
+  )
+}
+
+export default App

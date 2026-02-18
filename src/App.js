b5d950a46321c4f520f0e8341f0fc0510c9f160c
mariansilva <marian1996silva@gmail.com>
2026-02-18 11:28:29 +0530

    Added Header and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import NavBar from './components/navBar/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Cart from './pages/cart/Cart'
+import PlaceOrder from './pages/placeOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/placeOrder'element={<PlaceOrder/>}/>
+      </Routes>
       <title>Food Del</title>
     </div>
   )

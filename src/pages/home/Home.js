052c316f3f17854638e18b9ddf60d9eeae592390
mariansilva <marian1996silva@gmail.com>
2026-02-19 09:37:41 +0530

    Added Menu Items

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/header/Header'
+import ExploreMenu from '../../components/exploreMenu/ExploreMenu'
 
 const Home = () => {
+    const [category,setCategory] =useState("All");
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }

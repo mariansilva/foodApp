f57702950f173dfe6b625bded7b72eff42c94138
mariansilva <marian1996silva@gmail.com>
2026-03-09 13:43:01 +0530

    Added food Item

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/header/Header'
 import ExploreMenu from '../../components/exploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/foodDisplay/FoodDisplay'
 
 const Home = () => {
     const [category,setCategory] =useState("All");
@@ -9,8 +10,10 @@ const Home = () => {
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
 
 export default Home
+

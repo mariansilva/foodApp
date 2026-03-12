451974a97b895ac695eac168b7180734a94b7401
mariansilva <marian1996silva@gmail.com>
2026-03-12 19:13:01 +0530

    Added addToCart and removeFromCart Function in FoodItem

--- src/components/foodItem/FoodItem.js
+++ src/components/foodItem/FoodItem.js
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
-const FoodItem = ({id,name,price,description,image}) => {
-  return (
-    <div className='food-item'>
-        <div className='food-item-img-container'>
-            <img className='food-item-image'  src={image} alt=''/>
-        </div>
-        <div className='food-item-name-info'>
-            <div className='food-item-name-rating'>
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt=''/>
+import { StoreContext } from '../../context/StoreContext'
+const FoodItem = ({ id, name, price, description, image }) => {
+    
+    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+    return (
+        <div className='food-item'>
+            <div className='food-item-img-container'>
+                <img className='food-item-image' src={image} alt='' />
+                {!cartItems[id] ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt='' />
+                    : <div className='food-item-counter'>
+                        <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=''/>
+                        <p>{cartItems[id]}</p>
+                        <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt=''/>
+                    </div>}
             </div>
-            <p className='food-item-desc'>{description}</p>
-            <p className='food-item-price'>${price}</p>
+            <div className='food-item-name-info'>
+                <div className='food-item-name-rating'>
+                    <p>{name}</p>
+                    <img src={assets.rating_starts} alt='' />
+                </div>
+                <p className='food-item-desc'>{description}</p>
+                <p className='food-item-price'>${price}</p>
 
+
+            </div>
 
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default FoodItem

6d4fbd98d7e339b56f0c46fcdec745dbc6d3029c
mariansilva <marian1996silva@gmail.com>
2026-07-06 11:29:50 +0530

    Footer Component has completed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,19 +5,37 @@ import { assets } from '../../assets/assets'
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
-    <div className='footer-content'></div>
+    <div className='footer-content'>
       <div className='footer-content-left'>
         <img src={assets.logo} alt=''></img>
-        <p>Lorem Ipsum is simply dummy text of the printing and typinsgetting industry</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typinsgetting industry.Lorem lpsum has ben the industry's standard dummy text ever since 1500s, when on unkown printer took a gallery of type and scrambled it to make a type specimen book</p>
       <div className='footer-social-icons'>
-      <img src={assets.facebook_icon} alt=''></img>
-      <img src={assets.twitter_icon} alt=''></img>
-      <img src={assets.linkedin_icon} alt=''></img>
+      <img src={assets.facebook_icon} alt=''/>
+      <img src={assets.twitter_icon} alt=''/>
+      <img src={assets.linkedin_icon} alt=''/>
       </div>
       </div>
-    <div className='footer-content-center'></div>
-     <div className='footer-content-right'></div>
+    <div className='footer-content-center'>
+      <h2>COMPANY</h2>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Delivery</li>
+        <li>Privacy Policy</li>
+      </ul>
     </div>
+     <div className='footer-content-right'>
+      <h2>GET IN TOUCH</h2>
+      <ul>
+        <li>+1-212-656-7870</li>
+        <li>comback@tomatto.com</li>
+      </ul>
+     </div>
+    </div>
+    <hr/>
+    <p className='footer-copyright'>Copyright 2024 © Tomato.com - All Right Reserved</p>
+    </div>
+
    
   )
 }

e45c530d6542ebb522367c24250b266cffb6fad0
mariansilva <marian1996silva@gmail.com>
2026-08-06 13:05:14 +0530

    Completed Login User Function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,10 +2,30 @@ import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
+import { response } from "express";
 
 //login user
 const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.json({ success: false, message: "User does not exist" })
+        }
+        const isMath = await bcrypt.compare(password, user.password);
+        if (!isMath) {
+            return res.json({ success: false, message: "Invalid creadentials" })
+        }
+        const token = createToken(user._id);
+        res.json({ success: true, token })
+    }
 
+
+    catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+
+    }
 }
 const createToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET)
@@ -51,4 +71,4 @@ const registerUser = async (req, res) => {
     }
 }
 
-export { loginUser, registerUser}
+export { loginUser, registerUser }

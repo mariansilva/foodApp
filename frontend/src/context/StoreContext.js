4581126ee290a8013131433da4a146503a5e4623
mariansilva <marian1996silva@gmail.com>
2026-08-08 13:31:50 +0530

    Load data from the backend

--- frontend/src/context/StoreContext.js
+++ frontend/src/context/StoreContext.js
@@ -1,14 +1,15 @@
 import { createContext, useEffect } from "react";
-import { food_list } from "../assets/assets";
 import { useState } from "react";
+import axios from "axios"
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url ="http://localhost:4000";
-    const [token,setToken] = useState("")
+    const url = "http://localhost:4000";
+    const [token, setToken] = useState("");
+    const [food_list, setFoodList] = useState([]);
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -33,6 +34,21 @@ const StoreContextProvider = (props) => {
         }
         return totalAmount;
     }
+
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + "/api/food/list");
+        setFoodList(response.data.data);
+    }
+
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+    }, [])
 
     const contextValue = {
         food_list,

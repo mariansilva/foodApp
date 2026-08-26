b8af473f995581c303dfafa8e051e0530e3a1fc9
mariansilva <marian1996silva@gmail.com>
2026-08-26 06:46:59 +0530

    Typo error correction

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,11 +43,11 @@ const placeOrder = async (req, res) => {
         })
 
 
-        const session = await stripe.checkout.session.create({
+        const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: "payment",
-            success_url: `${dynamicFrontendUrl}/verify?scuccess=true&orderId=${newOrder._id}`,
-            cancel_url: `${dynamicFrontendUrl}/verify?scuccess=false&orderId=${newOrder._id}`,
+            success_url: `${dynamicFrontendUrl}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${dynamicFrontendUrl}/verify?success=false&orderId=${newOrder._id}`,
         })
         res.json({ success: true, session_url: session.url })
     } catch (error) {
